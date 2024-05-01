1de5dd5b21a440ef3135f41797bc541de38bd139
wenkai-li <wenkailileo@gmail.com>
2024-05-01 15:21:13 -0400

    update kyle

--- LLaMA-Factory/src/train_sft_lora.sh
+++ LLaMA-Factory/src/train_sft_lora.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 # MODEL_NAME_OR_PATH="/data/models/huggingface/meta-llama/Llama-2-7b-hf"
-MODEL_NAME_OR_PATH="mistralai/Mistral-7B-Instruct-v0.2"
-OUTPUT_DIR="/data/user_data/wenkail/mlsys/lora_test/mistral_7b_instruct_v0.2"
-
+# MODEL_NAME_OR_PATH="mistralai/Mistral-7B-Instruct-v0.2"
+MODEL_NAME_OR_PATH="meta-llama/Meta-Llama-3-8B-Instruct"
+# OUTPUT_DIR="/data/user_data/wenkail/mlsys/lora_test_v3/mistral_7b_instruct_v0.2"
+# OUTPUT_DIR="/data/user_data/wenkail/mlsys/lora_test_v3/llama_2_7b"
+OUTPUT_DIR="/data/user_data/wenkail/mlsys/lora_test_v4/llama_3_8b_instruct"
+# TEMPLATE="mistral"
+TEMPLATE="default"
 
 source ~/.bashrc
 cd ~/AMEFT/LLaMA-Factory/src
-conda activate llama_factory
+conda activate lf
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
     --config_file ../examples/accelerate/single_config.yaml \
     train_bash.py \
     --stage sft \
@@ -17,15 +21,14 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
     --model_name_or_path=$MODEL_NAME_OR_PATH \
     --dataset alpaca_news_summarization_train \
     --dataset_dir ../data \
-    --template default \
+    --template=$TEMPLATE \
     --finetuning_type lora \
     --lora_target q_proj,v_proj \
     --output_dir=$OUTPUT_DIR \
     --overwrite_cache \
-    --overwrite_output_dir \
-    --cutoff_len 2048 \
+    --cutoff_len 4096 \
     --preprocessing_num_workers 16 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
@@ -35,9 +38,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch \
     --eval_steps 100 \
     --evaluation_strategy steps \
     --load_best_model_at_end \
-    --learning_rate 5e-5 \
-    --num_train_epochs 10.0 \
+    --learning_rate 5e-4 \
+    --num_train_epochs 30.0 \
     --max_samples 1000 \
     --val_size 0.1 \
-    --plot_loss \
-    --fp16
+    --plot_loss 
+    # --fp16
+
